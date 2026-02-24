7aa078c331ed7935d3e71dc89ca93d33dee7639d
manish70158 <99177668+manish70158@users.noreply.github.com>
2026-02-24 18:42:58 +0530

    Initial commit: Indian Market Sentiment Analyzer

---
+++ run_sentiment_job.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Script to run the Indian Market Sentiment Analyzer and send email
+python3 /Users/manishkumar/Documents/learning/antigravity/research/market_sentiment_analyzer.py --email
